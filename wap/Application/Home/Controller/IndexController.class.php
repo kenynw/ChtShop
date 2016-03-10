ff618f28c6a2d61f1fb1d8732197cff103d5071d
chaxin <chaxinkeji@126.com>
2016-03-10 16:25:30 +0800

    网页商品详情不可用

--- wap/Application/Home/Controller/IndexController.class.php
+++ wap/Application/Home/Controller/IndexController.class.php
@@ -430,7 +430,7 @@ class IndexController extends Controller
         $this->assign('info', $info);
         $this->assign('brand_name', $brandname['brand_name']);
         $this->assign('evals', $eval);
-        $this->display('goods');
+        $this->display('good');
     }
 
     //登陆页
