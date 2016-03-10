a634fd26cf8d2baa2db3ab2fd5bba34baabe9ab4
chaxin <chaxinkeji@126.com>
2016-03-10 11:58:07 +0800

    修改购买第一步接口

--- mobile/control/member_buy.php
+++ mobile/control/member_buy.php
@@ -77,25 +77,23 @@ class member_buyControl extends mobileMemberControl {
         $store_cart_list = array();
         foreach ($result['store_cart_list'] as $key => $value) {
             if(isset($_POST['version']) && $_POST['version'] > 1) {
-                $store_cart_list['goods_list'] = $value;
-                $store_cart_list['store_goods_total'] = $result['store_goods_total'][$key];
+                $store = array();
+                $store['goods_list'] = $value;
+                $store['store_goods_total'] = $result['store_goods_total'][$key];
                 if(!empty($result['store_premiums_list'][$key])) {
                     $result['store_premiums_list'][$key][0]['premiums'] = true;
                     $result['store_premiums_list'][$key][0]['goods_total'] = 0.00;
                     $store_cart_list['goods_list'][] = $result['store_premiums_list'][$key][0];
                 }
-                $store_cart_list['store_mansong_rule_list'] = $result['store_mansong_rule_list'][$key];
-                $store_cart_list['store_voucher_list'] = $result['store_voucher_list'][$key];
+                $store['store_mansong_rule_list'] = $result['store_mansong_rule_list'][$key];
+                $store['store_voucher_list'] = $result['store_voucher_list'][$key];
                 if(!empty($result['cancel_calc_sid_list'][$key])){
-                    $store_cart_list['freight'] = '0';
-                    $store_cart_list['freight_message'] = $result['cancel_calc_sid_list'][$key]['desc'];
+                    $store['freight'] = '0';
+                    $store['freight_message'] = $result['cancel_calc_sid_list'][$key]['desc'];
                 } else {
-                    $store_cart_list['freight'] = '1';
+                    $store['freight'] = '1';
                 }
-                /*代金券 add by lai<<*/
-                $store_cart_list['voucher_list']=array_values($result['store_voucher_list'][$key]);
-                /*代金券>>*/
-                $store_cart_list['store_name'] = $value[0]['store_name'];
+                $store_cart_list[] = $store;
             } else {
                 $store_cart_list[$key]['goods_list'] = $value;
                 $store_cart_list[$key]['store_goods_total'] = $result['store_goods_total'][$key];
