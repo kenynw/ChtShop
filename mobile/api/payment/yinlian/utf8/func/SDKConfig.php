fd57e953b4848dc8f82e851d8a4ae1cf499c7d6e
chaxin <chaxinkeji@126.com>
2016-04-06 20:20:29 +0800

    恢复银联支付（mobile/api/payment/yinlian/）整个目录到旧服务器上的版本

--- mobile/api/payment/yinlian/utf8/func/SDKConfig.php
+++ mobile/api/payment/yinlian/utf8/func/SDKConfig.php
@@ -1,62 +1,45 @@
-﻿<?php
+<?php
 
 
-// cvn2加密 1：加密 0:不加密
 const SDK_CVN2_ENC = 0;
-// 有效期加密 1:加密 0:不加密
+
 const SDK_DATE_ENC = 0;
-// 卡号加密 1：加密 0:不加密
+
 const SDK_PAN_ENC = 0;
  
-// ######(以下配置为PM环境：入网测试环境用，生产环境配置见文档说明)#######
-// 签名证书路径
-const SDK_SIGN_CERT_PATH = './certs/PM_700000000000001_acp.pfx';
+const SDK_SIGN_CERT_PATH = 'C:/xampp/htdocs/shopncb2b2c/mobile/certs/PRO_700000000000001_acp.pfx';
 
-// 签名证书密码
- const SDK_SIGN_CERT_PWD = '000000';
+const SDK_SIGN_CERT_PWD = 'chaxin';
+ 
+const SDK_VERIFY_CERT_PATH = 'C:/xampp/htdocs/shopncb2b2c/mobile/certs/UPOP_VERIFY.cer';
 
-// 密码加密证书（这条用不到的请随便配）
-const SDK_ENCRYPT_CERT_PATH = './certs/verify_sign_acp.cer';
+const SDK_ENCRYPT_CERT_PATH = 'C:/xampp/htdocs/shopncb2b2c/mobile/certs/RSA2048_PROD_index_22.cer';
 
-// 验签证书路径（请配到文件夹，不要配到具体文件）
-const SDK_VERIFY_CERT_DIR = './certs/';
+const SDK_VERIFY_CERT_DIR = 'C:/xampp/htdocs/shopncb2b2c/mobile/certs/';
 
-// 前台请求地址
-const SDK_FRONT_TRANS_URL = 'https://101.231.204.80:5000/gateway/api/frontTransReq.do';
+const SDK_FRONT_TRANS_URL = 'https://gateway.95516.com/gateway/api/frontTransReq.do';
 
-// 后台请求地址
-const SDK_BACK_TRANS_URL = 'https://101.231.204.80:5000/gateway/api/backTransReq.do';
+const SDK_BACK_TRANS_URL = 'https://gateway.95516.com/gateway/api/backTransReq.do';
 
-// 批量交易
-const SDK_BATCH_TRANS_URL = 'https://101.231.204.80:5000/gateway/api/batchTrans.do';
+const SDK_BATCH_TRANS_URL = 'https://gateway.95516.com/gateway/api/batchTrans.do';
 
-//单笔查询请求地址
-const SDK_SINGLE_QUERY_URL = 'https://101.231.204.80:5000/gateway/api/queryTrans.do';
+const SDK_SINGLE_QUERY_URL = 'https://gateway.95516.com/gateway/api/queryTrans.do';
 
-//文件传输请求地址
-const SDK_FILE_QUERY_URL = 'https://101.231.204.80:9080/';
+const SDK_FILE_QUERY_URL = 'https://filedownload.95516.com/';
 
-//有卡交易地址
-const SDK_Card_Request_Url = 'https://101.231.204.80:5000/gateway/api/cardTransReq.do';
+const SDK_Card_Request_Url = 'https://gateway.95516.com/gateway/api/cardTransReq.do';
 
-//App交易地址
-const SDK_App_Request_Url = 'https://101.231.204.80:5000/gateway/api/appTransReq.do';
+const SDK_App_Request_Url = 'https://gateway.95516.com/gateway/api/appTransReq.do';
 
+const SDK_FRONT_NOTIFY_URL = 'http://www.chahuitong.com/mobile/api/payment/yinlian/call_back_url.php';
 
-// 前台通知地址 (商户自行配置通知地址)
-const SDK_FRONT_NOTIFY_URL = 'http://localhost:8085/upacp_sdk_php/demo/utf8/FrontReceive.php';
-// 后台通知地址 (商户自行配置通知地址)
-const SDK_BACK_NOTIFY_URL = 'http://114.82.43.123/upacp_sdk_php/demo/utf8/BackReceive.php';
+const SDK_BACK_NOTIFY_URL = 'http://www.chahuitong.com/mobile/api/payment/yinlian/notify_url.php';
 
-//文件下载目录 
-const SDK_FILE_DOWN_PATH = './file/';
+const SDK_FILE_DOWN_PATH = 'C:/file/';
 
-//日志 目录 
-const SDK_LOG_FILE_PATH = './logs/';
+const SDK_LOG_FILE_PATH = './logs';
 
-//日志级别
 const SDK_LOG_LEVEL = 'INFO';
-
 
 	
 ?>
