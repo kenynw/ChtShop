35e6bb409d367258e7f04f75498bc620b0f6499f
chaxin <chaxinkeji@126.com>
2016-04-19 17:07:44 +0800

    再次配置银联

--- mobile/api/payment/yinlian/utf8/func/SDKConfig.php
+++ mobile/api/payment/yinlian/utf8/func/SDKConfig.php
@@ -22,31 +22,31 @@ const SDK_ENCRYPT_CERT_PATH = './certs/verify_sign_acp.cer';
 const SDK_VERIFY_CERT_DIR = './certs/';
 
 // 前台请求地址
-const SDK_FRONT_TRANS_URL = 'https://101.231.204.80:5000/gateway/api/frontTransReq.do';
+const SDK_FRONT_TRANS_URL = 'https://gateway.95516.com/gateway/api/frontTransReq.do';
 
 // 后台请求地址
-const SDK_BACK_TRANS_URL = 'https://101.231.204.80:5000/gateway/api/backTransReq.do';
+const SDK_BACK_TRANS_URL = 'https://gateway.95516.com/gateway/api/backTransReq.do';
 
 // 批量交易
-const SDK_BATCH_TRANS_URL = 'https://101.231.204.80:5000/gateway/api/batchTrans.do';
+const SDK_BATCH_TRANS_URL = 'https://gateway.95516.com/gateway/api/batchTrans.do';
 
 //单笔查询请求地址
-const SDK_SINGLE_QUERY_URL = 'https://101.231.204.80:5000/gateway/api/queryTrans.do';
+const SDK_SINGLE_QUERY_URL = 'https://gateway.95516.com/gateway/api/queryTrans.do';
 
 //文件传输请求地址
-const SDK_FILE_QUERY_URL = 'https://101.231.204.80:9080/';
+const SDK_FILE_QUERY_URL = 'https://filedownload.95516.com/';
 
 //有卡交易地址
-const SDK_Card_Request_Url = 'https://101.231.204.80:5000/gateway/api/cardTransReq.do';
+const SDK_Card_Request_Url = 'https://gateway.95516.com/gateway/api/cardTransReq.do';
 
 //App交易地址
-const SDK_App_Request_Url = 'https://101.231.204.80:5000/gateway/api/appTransReq.do';
+const SDK_App_Request_Url = 'https://gateway.95516.com/gateway/api/appTransReq.do';
 
 
 // 前台通知地址 (商户自行配置通知地址)
-const SDK_FRONT_NOTIFY_URL = 'http://localhost:8085/upacp_sdk_php/demo/utf8/FrontReceive.php';
+const SDK_FRONT_NOTIFY_URL = 'http://www.chahuitong.com/mobile/api/payment/yinlian/call_back_url.php';
 // 后台通知地址 (商户自行配置通知地址)
-const SDK_BACK_NOTIFY_URL = 'http://114.82.43.123/upacp_sdk_php/demo/utf8/BackReceive.php';
+const SDK_BACK_NOTIFY_URL = 'http://www.chahuitong.com/mobile/api/payment/yinlian/notify_url.php';
 
 //文件下载目录
 const SDK_FILE_DOWN_PATH = './file/';
