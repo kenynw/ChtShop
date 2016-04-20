1b93fe05a73b5059fc66c4dfa5b8024fc37b0aa2
chaxin <chaxinkeji@126.com>
2016-04-20 13:58:14 +0800

    限时抢购数据结构修改

--- mobile/control/xianshi_goods.php
+++ mobile/control/xianshi_goods.php
@@ -23,16 +23,24 @@ class xianshi_goodsControl extends mobileHomeControl {
     public function current_listOp() {
         $model_xianshi_goods = Model('p_xianshi_goods');
 
+        $field = 'xianshi_goods_id,xianshi_id,goods_id,store_id,xianshi_price,goods_name,goods_image,goods_price,start_time,end_time,state,xianshi_recommend';
+
+        $order = 'xianshi_recommend desc';
+
         $condition = array();
         $condition['state'] = self::XIANSHI_STATE_NORMAL;
         $condition['start_time'] = array('lt', TIMESTAMP);
         $condition['end_time'] = array('gt', TIMESTAMP);
         $condition['xianshi_name'] = self::XIANSHI_NAME_CURRENT;
-        $xianshi_goods_list = $model_xianshi_goods -> getXianshiGoodsList($condition, $this->page);
+        $xianshi_goods_list = $model_xianshi_goods -> getXianshiGoodsList($condition, $this->page, $order, $field);
 
         // 如果后台没有及时设置商品,获取所有抢购商品
         if(empty($xianshi_goods_list)) {
-            $condition['xianshi_name'] = '';
+            $condition = array(
+                'state' => self::XIANSHI_STATE_NORMAL,
+                'start_time' => array('lt', TIMESTAMP),
+                'end_time' => array('gt', TIMESTAMP),
+            );
             $xianshi_goods_list = $model_xianshi_goods -> getXianshiGoodsList($condition,$this->page);
         }
 
@@ -44,7 +52,9 @@ class xianshi_goodsControl extends mobileHomeControl {
             $xianshi_goods_list[$key]['goods_image_url'] = cthumb($goods['goods_image'], 360, $goods['store_id']);
         }
 
-        output_json(1, $xianshi_goods_list);
+        $page_count = $model_xianshi_goods -> gettotalpage();
+
+        output_json(1, array('list' => $xianshi_goods_list), 'SUCCESS', mobile_page($page_count));
     }
 
 }
