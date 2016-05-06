f477ba99bca8002262211b9eed04e6ca2dccfc3b
chaxin <chaxinkeji@126.com>
2016-05-06 11:23:18 +0800

    兼容旧版

--- wap/js/tmpl/order_delivery.js
+++ wap/js/tmpl/order_delivery.js
@@ -1,5 +1,5 @@
 $(function() {
-    var key = $.cookie("key");
+    var key = getcookie("key");
     // if (key=='') {
     //     if (navigator.userAgent.indexOf("android") != -1) {
     //         key = window.CHTAndroid.getToken();
