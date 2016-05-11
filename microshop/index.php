06701af67fec27d458af267f6889c9144a9a76c9
chaxin <chaxinkeji@126.com>
2016-05-11 18:47:47 +0800

    配置微商城

--- microshop/index.php
+++ microshop/index.php
@@ -13,7 +13,7 @@
 define('APP_ID','microshop');
 define('BASE_PATH',str_replace('\\','/',dirname(__FILE__)));
 if (!@include(dirname(dirname(__FILE__)).'/global.php')) exit('global.php isn\'t exists!');
-if (!@include(BASE_CORE_PATH.'/shopnc.php')) exit('shopnc.php isn\'t exists!');
+if (!@include(BASE_CORE_PATH.'/33hao.php')) exit('33hao.php isn\'t exists!');
 
 if (!@include(BASE_PATH.'/config/config.ini.php')){
 	@header("Location: install/index.php");die;
