5ef0daaa8eddc79466e15e0182aaa48aab9bbe18
chaxin <chaxinkeji@126.com>
2016-06-17 22:50:40 +0800

    修改商城index

--- chahuitong.com/www/shop/index.php
+++ chahuitong.com/www/shop/index.php
@@ -7,7 +7,7 @@
 define('APP_ID','shop');
 define('BASE_PATH',str_replace('\\','/',dirname(__FILE__)));
 //echo BASE_PATH;
-if (!@include(dirname(dirname(__FILE__)).'/global.php')) exit('global.php isn\'t exists!');
+if (!@include(dirname(dirname(dirname(__FILE__))).'/global.php')) exit('global.php isn\'t exists!');
 if (!@include(BASE_PATH.'/control/control.php')) exit('control.php isn\'t exists!');
 if (!@include(BASE_CORE_PATH.'/33hao.php')) exit('33hao.php isn\'t exists!');
 define('APP_SITE_URL',SHOP_SITE_URL);
