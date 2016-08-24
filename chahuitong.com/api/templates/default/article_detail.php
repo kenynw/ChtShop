6b73010eee604e5d0b9c326a9fd0aae11d07405b
chaxin <program@chahuitong.com>
2016-08-24 14:33:00 +0800

    change article css

--- chahuitong.com/api/templates/default/article_detail.php
+++ chahuitong.com/api/templates/default/article_detail.php
@@ -3,7 +3,8 @@
 <html>
 <head>
     <meta http-equiv="content-type" content="text/html; charset= <?php echo CHARSET;?> " />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0,user-scalable=0">
     <meta name="apple-mobile-web-app-capable" content="yes">
     <meta name="apple-mobile-web-app-status-bar-style" content="black">
     <meta name="format-detection" content="telephone=no">
